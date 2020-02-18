f85cceed33e0b0a9bf4a3d2169a42f732017bae6
willnaoosmit <55159911+willnaoosmit@users.noreply.github.com>
2020-02-18 14:20:19 -0300

    Update libs.sh

--- libs.sh
+++ libs.sh
@@ -10,6 +10,6 @@ if ! type "tar" > /dev/null; then
     exit 1; 
 fi
 
-curl -LO https://github.com/datso/react-native-pjsip-builder/releases/download/v2.7.1-with-vialer/release.tar.gz
+curl -LO https://github.com/willnaoosmit/react-native-pjsip-builder/archive/v2.8.0.tar.gz
 tar -xvf release.tar.gz
 rm release.tar.gz
